76bbc913eae2219d7cdf1215e6bd9295463dac56
IamShaDoW666 <www.gaming2018@gmail.com>
2023-09-01 02:06:38 +0530

    update UI of gallery screen

--- lib/screens/booking/booking_gallery_screen.dart
+++ lib/screens/booking/booking_gallery_screen.dart
@@ -24,63 +24,42 @@ class _BookingGalleryScreenState extends State<BookingGalleryScreen> {
   Widget build(BuildContext context) {
     if (widget.imageList!.isEmpty) return Text("No image");
     return Scaffold(
-      appBar: appBarWidget(
-        "Gallery",
-        textColor: Colors.white,
-        textSize: APP_BAR_TEXT_SIZE,
-        color: primaryColor,
-        systemUiOverlayStyle: SystemUiOverlayStyle(
-            statusBarIconBrightness:
-                appStore.isDarkMode ? Brightness.light : Brightness.light,
-            statusBarColor: context.primaryColor),
-        showBack: Navigator.canPop(context),
-        backWidget: BackWidget(),
-      ),
-      body: AnimatedScrollView(
-        onSwipeRefresh: () async {
-          await init();
-          setState(() {});
-          await 2.seconds.delay;
-        },
-        physics: AlwaysScrollableScrollPhysics(),
-        padding: EdgeInsets.all(16),
-        listAnimationType: ListAnimationType.FadeIn,
-        fadeInConfiguration: FadeInConfiguration(duration: 2.seconds),
-        children: [
-          AnimatedWrap(
-            runSpacing: 16,
-            spacing: 8,
+        appBar: appBarWidget(
+          "Gallery",
+          textColor: Colors.white,
+          textSize: APP_BAR_TEXT_SIZE,
+          color: primaryColor,
+          systemUiOverlayStyle: SystemUiOverlayStyle(
+              statusBarIconBrightness:
+                  appStore.isDarkMode ? Brightness.light : Brightness.light,
+              statusBarColor: context.primaryColor),
+          showBack: Navigator.canPop(context),
+          backWidget: BackWidget(),
+        ),
+        body: GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
             itemCount: widget.imageList!.length,
-            listAnimationType: ListAnimationType.FadeIn,
-            fadeInConfiguration: FadeInConfiguration(duration: 2.seconds),
-            scaleConfiguration: ScaleConfiguration(
-                duration: 300.milliseconds, delay: 50.milliseconds),
-            itemBuilder: (_, i) {
-              return GestureDetector(
-                onTap: () {
-                  if (widget.imageList![i].isNotEmpty)
-                    ZoomImageScreen(
-                            galleryImages: [widget.imageList![i]], index: 0)
-                        .launch(context);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(blurRadius: 16, color: Colors.black12)
-                  ]),
-                  child: CachedImageWidget(
-                    url: widget.imageList![i].validate(),
-                    fit: BoxFit.cover,
-                    width: 100,
-                    height: 100,
-                    placeHolderImage: '',
+            itemBuilder: (_, i) => GestureDetector(
+                  onTap: () {
+                    if (widget.imageList![i].isNotEmpty)
+                      ZoomImageScreen(
+                              galleryImages: [widget.imageList![i]], index: 0)
+                          .launch(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(blurRadius: 16, color: Colors.black12)
+                    ]),
+                    child: CachedImageWidget(
+                      url: widget.imageList![i].validate(),
+                      fit: BoxFit.cover,
+                      width: 100,
+                      height: 100,
+                      placeHolderImage: '',
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
+                )));
   }
 }
