e196f91865396011f42e6debdf37586d672e42ad
IamShaDoW666 <www.gaming2018@gmail.com>
2023-11-01 10:01:41 +0530

    config

--- lib/utils/configs.dart
+++ lib/utils/configs.dart
@@ -1,11 +1,11 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 
-const APP_NAME = 'Fixit';
+const APP_NAME = 'Yallakhedma';
 const APP_NAME_TAG_LINE = 'On-Demand Services App';
 var defaultPrimaryColor = Color.fromARGB(255, 181, 93, 31);
 
-const DOMAIN_URL = 'http://192.168.18.38:8000';
+const DOMAIN_URL = 'https://yallakhedma.com';
 const BASE_URL = '$DOMAIN_URL/api/';
 
 const DEFAULT_LANGUAGE = 'en';
@@ -24,14 +24,14 @@ const DASHBOARD_AUTO_SLIDER_SECOND = 5;
 
 const TERMS_CONDITION_URL = 'https://yallakhedma.com';
 const PRIVACY_POLICY_URL = 'https://yallakhedma.com';
-const INQUIRY_SUPPORT_EMAIL = 'hello@iqonic.design';
+const INQUIRY_SUPPORT_EMAIL = 'support@yallakhedma.com';
 
 /// You can add help line number here for contact. It's demo number
-const HELP_LINE_NUMBER = '+15265897485';
+const HELP_LINE_NUMBER = '+971565002940';
 
 /// STRIPE PAYMENT DETAIL
-const STRIPE_MERCHANT_COUNTRY_CODE = 'IN';
-const STRIPE_CURRENCY_CODE = 'INR';
+const STRIPE_MERCHANT_COUNTRY_CODE = 'UAE';
+const STRIPE_CURRENCY_CODE = 'DHS';
 DateTime todayDate = DateTime(2022, 8, 24);
 
 /// SADAD PAYMENT DETAIL
@@ -40,16 +40,16 @@ const SADAD_PAY_URL = "https://d.sadad.qa";
 
 Country defaultCountry() {
   return Country(
-    phoneCode: '91',
-    countryCode: 'IN',
-    e164Sc: 91,
+    phoneCode: '971',
+    countryCode: 'UAE',
+    e164Sc: 971,
     geographic: true,
     level: 1,
-    name: 'India',
-    example: '9123456789',
-    displayName: 'India (IN) [+91]',
-    displayNameNoCountryCode: 'India (IN)',
-    e164Key: '91-IN-0',
-    fullExampleWithPlusSign: '+919123456789',
+    name: 'United Arab Emirates',
+    example: '971565002940',
+    displayName: 'United Arab Emirates (UAE) [+971]',
+    displayNameNoCountryCode: 'United Arab Emirates (UAE)',
+    e164Key: '971-UAE-0',
+    fullExampleWithPlusSign: '+971565002940',
   );
 }
